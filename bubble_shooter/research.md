7e0af34f8898a90e981785fdbf29cc328baa8272
Tran Hoang Tran <tranth@qts-smartsoftware.vn>
2025-09-05 14:18:36 +0700

    Snap bubble to grid

--- bubble_shooter/research.md
+++ bubble_shooter/research.md
@@ -2,3 +2,32 @@
 
 - Vite + Typescript: khởi động nhanh, cấu hình nhẹ.
 - Phaser: một HTML5 game framwork mã nguồn mở.Phaser sử dụng Pixi.js để rendering trên WebGL và Canvas.
+
+# Thông tin Game Bubble Shooter
+
+## Hexagon grid
+
+- Game sử dụng bố cục hexagon grid.
+- Ưu điểm:
+
+* Các quả bóng xếp khít nhau theo dạng tổ ong.
+* Hỗ trợ cơ chế rơi xuống tự nhiên.
+* Dễ tính toán va chạm và gắn kết.
+
+## Mục tiêu
+
+- Người chơi điều khiển shooter ở cuối màn hình.
+- Bắn bóng lên phía trên để ghép thành cụm >= 3 bóng cùng màu.
+- Trò chơi kết thúc khi game chạm đáy.
+
+## Cơ chế game:
+
+- Bắn bóng: người chơi chọn hướng và bắn.
+- Va chạm: bóng sẽ gắn vào lưới gần nhất.
+- Ghép nhóm: lớn hơn 3 bóng cùng màu thì biến mất.
+
+## Giao diện:
+
+- Shooter: nằm ở giũa phía dưới.
+- Lưới bóng: hiển thị phía trên.
+- Thanh điểm.
--- bubble_shooter/src/GameScene.ts
+++ bubble_shooter/src/GameScene.ts
@@ -22,13 +22,28 @@ export default class GameScene extends Phaser.Scene {
 
   private currentBubbleColor!: number;
   private colors = [0xf97070, 0x70f9a0, 0x709ff9, 0xf9f970];
-  private speed = 300;
+  private speed = 600;
+
+  //Variable grid
+  private grid: { color: number }[][] = [];
+  private COLS = 11;
+  private ROWS = 13;
+  private TOP_PAD = 60;
+  private ROW_H = Math.floor(this.bubbleRadius * Math.sqrt(3));
+  private LEFT_PAD!: number;
+  private RIGHT_PAD!: number;
+  private gridGraphics!: Phaser.GameObjects.Graphics;
 
   constructor() {
     super("game");
   }
 
   create() {
+    this.LEFT_PAD = Math.floor(
+      (this.scale.width - this.COLS * this.bubbleRadius * 2) / 2
+    );
+    this.RIGHT_PAD = Math.floor(this.scale.width - this.LEFT_PAD);
+
     // shooter
     this.shooterX = this.scale.width / 2;
     this.shooterY = this.scale.height - 48;
@@ -54,6 +69,130 @@ export default class GameScene extends Phaser.Scene {
     this.input.on("pointerdown", (pointer: Phaser.Input.Pointer) => {
       this.shootBubble(pointer.x, pointer.y);
     });
+
+    // draw grid
+
+    this.gridGraphics = this.add.graphics();
+
+    for (let row = 0; row < this.ROWS; row++) {
+      this.grid[row] = [];
+      // for (let col = 0; col < this.COLS; col++) {
+      //   this.grid[row][col] = { color: 0xffffff };
+      // }
+    }
+
+    this.drawGrid();
+  }
+
+  private drawGrid() {
+    this.gridGraphics.clear();
+    for (let row = 0; row < this.ROWS; row++) {
+      for (let col = 0; col < this.COLS; col++) {
+        const cell = this.grid[row][col];
+        if (!cell) continue;
+        const x =
+          this.LEFT_PAD +
+          col * this.bubbleRadius * 2 +
+          this.bubbleRadius +
+          (row % 2 === 1 ? this.bubbleRadius : 0);
+        const y = this.TOP_PAD + row * this.ROW_H + this.bubbleRadius;
+        this.gridGraphics.fillStyle(cell.color, 1);
+        this.gridGraphics.fillCircle(x, y, this.bubbleRadius);
+      }
+    }
+  }
+
+  //Convert row and col to coordinates in canva
+  private cellToWorld(row: number, col: number) {
+    const xOffset = row % 2 === 0 ? 0 : this.bubbleRadius;
+    const x =
+      col * this.bubbleRadius * 2 + xOffset + this.LEFT_PAD + this.bubbleRadius;
+    const y = row * this.ROW_H + this.TOP_PAD + this.bubbleRadius;
+    return { x, y };
+  }
+  //Return neighbors of bubble already exist
+  private getNeighbors(row: number, col: number) {
+    const even = row % 2 === 0;
+    const dist = even
+      ? [
+          [-1, 0],
+          [-1, -1],
+          [0, -1],
+          [0, 1],
+          [1, 0],
+          [1, -1],
+        ]
+      : [
+          [-1, 0],
+          [-1, 1],
+          [0, -1],
+          [0, 1],
+          [1, 0],
+          [1, 1],
+        ];
+
+    return dist
+      .map(([dr, dc]) => [row + dr, col + dc])
+      .filter(([r, c]) => r >= 0 && r < this.ROWS && c >= 0 && c < this.COLS);
+  }
+
+  private findBestEmptyNeighbor(row: number, col: number, bubble: any) {
+    let best = null;
+    let minDist = Number.MAX_VALUE;
+
+    for (const [nr, nc] of this.getNeighbors(row, col)) {
+      if (this.grid[nr][nc]) continue;
+      const { x, y } = this.cellToWorld(nr, nc);
+      const dx = bubble.x - x;
+      const dy = bubble.y - y;
+      const dist = Math.sqrt(dx * dx + dy * dy);
+      if (dist < minDist) {
+        minDist = dist;
+        best = { row: nr, col: nc, x, y };
+      }
+    }
+    return best;
+  }
+
+  // private getGridPosition(x: number, y: number) {
+  //   //row nearest
+  //   const row = Math.floor((y - this.TOP_PAD) / this.ROW_H);
+  //   //offset
+  //   const rowOffset = row % 2 === 1 ? this.bubbleRadius : 0;
+  //   //col nearest
+  //   const col = Math.floor(
+  //     (x - this.LEFT_PAD - rowOffset) / (this.bubbleRadius * 2)
+  //   );
+  //   return { row, col };
+  // }
+
+  private placeBubbleToGrid(
+    bubble: typeof this.activeBubble,
+    row?: number,
+    col?: number
+  ) {
+    if (!bubble) return;
+
+    // const { row, col } = this.getGridPosition(bubble.x, bubble.y);
+
+    if (row === undefined || col === undefined) {
+      row = Math.floor((bubble.y - this.TOP_PAD) / this.ROW_H);
+      const rowOffset = row % 2 === 0 ? 0 : this.bubbleRadius;
+      col = Math.floor(
+        (bubble.x - this.LEFT_PAD + rowOffset) / (this.bubbleRadius * 2)
+      );
+    }
+
+    if (row >= 0 && row < this.ROWS && col >= 0 && col <= this.COLS) {
+      this.grid[row][col] = { color: bubble.color };
+    }
+
+    // delete bubble flying
+    bubble.gfx.destroy();
+    this.activeBubble = undefined;
+
+    // redraw grid
+    this.drawGrid();
   }
 
   private spawnNewBubble() {
@@ -123,6 +262,34 @@ export default class GameScene extends Phaser.Scene {
     this.spawnNewBubble();
   }
 
+  private checkCollisionWithGrid() {
+    if (!this.activeBubble) return;
+
+    for (let row = 0; row < this.ROWS; row++) {
+      for (let col = 0; col < this.COLS; col++) {
+        const cell = this.grid[row][col];
+        if (!cell) continue;
+
+        const { x, y } = this.cellToWorld(row, col);
+        const dx = this.activeBubble.x - x;
+        const dy = this.activeBubble.y - y;
+        const dist = Math.sqrt(dx * dx + dy * dy);
+
+        if (dist <= this.bubbleRadius * 2 - 2) {
+          const best = this.findBestEmptyNeighbor(row, col, this.activeBubble);
+          if (best) {
+            this.placeBubbleToGrid(
+              { ...this.activeBubble, x: best.x, y: best.y },
+              best.row,
+              best.col
+            );
+          }
+          return;
+        }
+      }
+    }
+  }
+
   update(time: number, delta: number) {
     if (!this.activeBubble) return;
 
@@ -141,16 +308,10 @@ export default class GameScene extends Phaser.Scene {
       this.activeBubble.r
     );
 
-    //if bubble touch the top edge
-    if (this.activeBubble.y - this.activeBubble.r <= 0) {
-      this.activeBubble.velocityX = 0;
-      this.activeBubble.velocityY = 0;
-    }
-
     // if bubble touch the left/right edge
     if (
-      this.activeBubble.x - this.activeBubble.r <= 0 ||
-      this.activeBubble.x + this.activeBubble.r >= this.scale.width
+      this.activeBubble.x - this.activeBubble.r <= this.LEFT_PAD ||
+      this.activeBubble.x + this.activeBubble.r >= this.RIGHT_PAD
     ) {
       this.activeBubble.velocityX *= -1;
     }
@@ -159,5 +320,15 @@ export default class GameScene extends Phaser.Scene {
     if (this.activeBubble.y + this.activeBubble.r >= this.scale.height) {
       this.activeBubble.velocityY *= -1;
     }
+
+    //if bubble touch the top edge
+    if (this.activeBubble.y - this.activeBubble.r <= this.TOP_PAD) {
+      // this.activeBubble.velocityX = 0;
+      // this.activeBubble.velocityY = 0;
+      // this.activeBubble.y = this.TOP_PAD + this.activeBubble.r;
+      this.placeBubbleToGrid(this.activeBubble);
+    }
+
+    this.checkCollisionWithGrid();
   }
 }
